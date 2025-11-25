25a22bebdfb214d786a2b62b789563d8e083c100
Marcos <markitosanches@gmail.com>
2025-11-25 16:22:28 -0500

    Class 8

---
+++ ecommerce/server/app/controllers/user.controller.js
@@ -0,0 +1,80 @@
+const db = require('../models')
+const User = db.users
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const https = require('http')
+
+exports.findAll = (req, res) => {
+    User.findAll()
+    .then(data => {
+        res.send(data)
+    })
+}
+
+exports.create = async (req, res) => {
+    if(!req.body.fullname || !req.body.email || !req.body.password){
+        res.status(400).send({
+            message: "User must have name, email and password"
+        })
+        return;
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+    //console.log(hashPassword)
+    await User.create({
+        fullname: req.body.fullname,
+        email: req.body.email,
+        password: hashPassword,
+    })
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 'Could not insert the data'
+        })
+    })
+}
+
+exports.findOne = async (req, res) => {
+  const user = await User.findOne({where: {email:req.body.email}})
+
+  if(!user){
+    return res.status(400).send({
+        message: 'Username not found'
+    })
+  }
+
+  if(!await bcrypt.compare(req.body.password, user.password)){
+    return res.status(400).send({
+        message: 'Password incorrect'
+    })
+  }
+
+    // console.log(user)
+    const token = jwt.sign({id: user.id}, 'secret')
+    res.cookie('jwt', token,{
+        httpOnly: true,
+        maxAge: 24 * 60 *60 * 1000
+    })
+
+    user.update({
+        token: token
+    })
+    const {password, ...data} = await user.toJSON()
+    res.send({
+        user: data
+    })
+}
+
+exports.logout = async (req, res) => {
+    res.cookie('jwt', '', {maxAge:0})
+    res.send({
+        message: 'Logout'
+    })
+}
+
+exports.auth = async (req, res) => {
+
+}
--- ecommerce/server/server.js
+++ ecommerce/server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const cookieParser = require('cookie-parser')
 
 const corsOption = {
     origin: 'http://localhost:5173',
@@ -12,7 +13,7 @@ app.use(cors(corsOption))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(cookieParser())
 app.get('/abc', (req, res) => {
     res.json({message: 'Welcome'})
 })
@@ -20,6 +21,7 @@ const db = require('./app/models')
 db.connex.sync()
 
 require('./app/routes/product.route')(app)
+require('./app/routes/user.route')(app)
 const PORT = 8080
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}.`)
